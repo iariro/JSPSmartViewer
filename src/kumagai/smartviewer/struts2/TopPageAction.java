78f11a5206b38efe4d99c592036076e4d4779792
kumagai <kumagai@toshiba-tops.co.jp>
2017-04-05 13:15:05 +0900

    new

---
+++ src/kumagai/smartviewer/struts2/TopPageAction.java
@@ -0,0 +1,66 @@
+package kumagai.smartviewer.struts2;
+
+import java.io.*;
+import java.util.*;
+import javax.servlet.*;
+import org.apache.struts2.*;
+import org.apache.struts2.convention.annotation.*;
+import kumagai.smartviewer.*;
+
+/**
+ * トップページ表示アクション。
+ * @author kumagai
+ */
+@Namespace("/smartviewer")
+@Results
+({
+	@Result(name="success", location="/smartviewer/index.jsp"),
+	@Result(name="error", location="/smartviewer/error.jsp")
+})
+public class TopPageAction
+{
+	public TreeMap<Integer, String> attributes = new TreeMap<Integer, String>();
+	
+	/**
+	 * カレント値表示アクション。
+	 * @return 処理結果
+	 */
+	@Action("index")
+	public String execute()
+		throws Exception
+	{
+		ServletContext context = ServletActionContext.getServletContext();
+		String smartFilePath = context.getInitParameter("SmartFilePath");
+		if (smartFilePath != null)
+		{
+			String [] filenames = new File(smartFilePath).list();
+			if (filenames != null && filenames.length > 0)
+			{
+				// ファイルは１個でもある
+
+				// 最新を見る
+				String filename = filenames[filenames.length - 1];
+				File file = new File(smartFilePath, filename);
+				FileInputStream stream = new FileInputStream(file);
+				int size = (int)file.length();
+				byte [] data = new byte [size];
+				stream.read(data);
+				stream.close();
+
+				SmartDataList smartDataList = new SmartDataList(data);
+
+				if (smartDataList.size() > 0)
+				{
+					SmartData smartData = smartDataList.get(smartDataList.size() - 1);
+					
+					for (SmartAttribute attribute : smartData.attributes)
+					{
+						attributes.put(attribute.getId(), String.format("%02X", attribute.getId()));
+					}
+				}
+			}
+		}
+
+		return "success";
+	}
+}
