c0528b84de479f2577b7d91389d28af77db4810c
kumagai <kumagai@toshiba-tops.co.jp>
2018-11-12 10:55:07 +0900

    SMART属性名拡充

--- src/kumagai/smartviewer/SmartAttributeTable.java
+++ src/kumagai/smartviewer/SmartAttributeTable.java
@@ -1,6 +1,6 @@
 package kumagai.smartviewer;
 
-import java.util.*;
+import java.util.TreeMap;
 
 /**
  * 属性名テーブル
@@ -42,6 +42,7 @@ public class SmartAttributeTable
 		put(3,		"Spin Up Time");
 		put(4,		"Start/Stop Count");
 		put(5,		"Reallocated Sectors Count");
+		put(6,		"Read Channel Margin");
 		put(7,		"Seek Error Rate");
 		put(8,		"Seek Time Performance");
 		put(9,		"Power-On Hours");
@@ -49,6 +50,32 @@ public class SmartAttributeTable
 		put(11,		"Recalibration Retries");
 		put(12,		"Device Power Cycle Count");
 		put(13,		"Soft Read Error Rate");
+		put(22,		"Current Helium Level");
+		put(160,	"Uncorrectable_Sector_Count");
+		put(161,	"Valid_Spare_Block");
+		put(165,	"Maximum_Erase_Count");
+		put(167,	"Average_Erase_Count");
+		put(169,	"Remaining life left");
+		put(170,	"Available Reserved Space");
+		put(171,	"SSD Program Fail Count");
+		put(172,	"SSD Erase Fail Count");
+		put(173,	"SSD Wear Leveling Count");
+		put(174,	"Unexpected power loss count");
+		put(175,	"Power Loss Protection Failure");
+		put(176,	"Erase Fail Count");
+		put(177,	"Wear Range Delta");
+		put(179,	"Used Reserved Block Count Total");
+		put(180,	"Unused Reserved Block Count Total");
+		put(181,	"Program Fail Count Total or Non-4K Aligned Access Count");
+		put(182,	"Erase Fail Count");
+		put(183,	"SATA Downshift Error Count or Runtime Bad Block");
+		put(184,	"End-to-End error / IOEDC");
+		put(185,	"Head Stability");
+		put(186,	"Induced Op-Vibration Detection");
+		put(187,	"Reported Uncorrectable Errors");
+		put(188,	"Command Timeout");
+		put(189,	"High Fly Writes");
+		put(190,	"Temperature Difference or Airflow Temperature");
 		put(191,	"G-sense Error Rate");
 		put(192,	"Power-off Retract Count");
 		put(193,	"Load/Unload Cycle Count");
@@ -76,11 +103,25 @@ public class SmartAttributeTable
 		put(222,	"Loaded Hours");
 		put(223,	"Load/Unload Retry Count");
 		put(224,	"Load Friction");
+		put(225,	"Load/Unload Cycle Count");
 		put(226,	"Load-in Time");
 		put(227,	"Torque Amplification Count");
 		put(228,	"Power-Off Retract Count");
 		put(230,	"GMR Head Amplitude");
+		put(231,	"Life Left (SSD) or Temperature");
+		put(232,	"Endurance Remaining or Available Reserved Space");
+		put(233,	"Media Wearout Indicator (SSD) or Power-On Hours ");
+		put(234,	"Average erase count AND Maximum Erase Count ");
+		put(235,	"Good Block Count AND System(Free) Block Count ");
 		put(240,	"Head Flying Hours");
+		put(241,	"Total LBAs Written");
+		put(242,	"Total LBAs Read");
+		put(243,	"Total LBAs Written Expanded");
+		put(244,	"Total LBAs Read Expanded");
+		put(249,	"NAND Writes (1GiB)");
 		put(250,	"Read Error Retry Rate");
+		put(251,	"Minimum Spares Remaining");
+		put(252,	"Newly Added Bad Flash Block");
+		put(254,	"Free Fall Protection");
 	}
 }
