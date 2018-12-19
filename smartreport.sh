7f11e9be31039e27fd642f9c6120054107a614d0
kumagai <kumagai@toshiba-tops.co.jp>
2018-12-19 21:54:35 +0900

    １ホスト複数ディスク対応

--- smartreport.sh
+++ smartreport.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-DATETIME=$(/Users/kumagai/Documents/bin/smartmontools-6.5/smartctl -a /dev/disk1 | curl -X POST -H "Content-Type: text/plain" http://192.168.10.10:8080/kumagai/receivesmartctloutput --data-binary @-)
-(echo $DATETIME ; df) | curl -X POST -H "Content-Type: text/plain" http://192.168.10.10:8080/kumagai/receivedfoutput --data-binary @-
+DATETIME=$( (echo 'dino2_sda' ; ./smartctl -a /dev/sda) | curl -X POST -H "Expect:" -H "Content-Type: text/plain" http://172.17.28.106:8080/kumagai/receivesmartctloutput --data-binary @-)
+(echo 'dino2_sda' ; echo $DATETIME ; df) | curl -X POST -H "Expect:" -H "Content-Type: text/plain" http://172.17.28.106:8080/kumagai/receivedfoutput --data-binary @-
